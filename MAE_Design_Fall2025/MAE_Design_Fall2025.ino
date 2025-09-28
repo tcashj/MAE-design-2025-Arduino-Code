6ed6942aace2221972be17f25070e6ee94200e82
tcashj <tanner.jacob@okstate.edu>
2025-09-27 19:12:58 -0500

    Create MAE_Design_Fall2025.ino

---
+++ MAE_Design_Fall2025/MAE_Design_Fall2025.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+Collaborators can view? check
+
+arduino to github? Check
+
+Next step is to find out branchs?
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
