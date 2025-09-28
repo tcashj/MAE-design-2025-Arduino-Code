135dcb942fe860bc36547dc16459ad5be9827cda
tcashj <tanner.jacob@okstate.edu>
2025-09-27 19:48:50 -0500

    Update MAE_Design_Fall2025.ino

--- MAE_Design_Fall2025/MAE_Design_Fall2025.ino
+++ MAE_Design_Fall2025/MAE_Design_Fall2025.ino
@@ -11,18 +11,38 @@ The next step is to find out the branches.
           Pull requests are for others to review/see what changes you made to the code before adding it to the main branch
 
 Steps to set up
-    1. download github desktop and arduino ide
-    2. Open github website and then click Code (should be a green button around the mid top right-ish area)
-    3. Click open with GitHub Desktop (this clones the code from the website to the app)
-          * should take a little bit to load
-    4. Open GitHub Desktop if it doesn't open automatically
-    5. I use the "view files in your repository" option since the first option options up a blank project and 
-       Then I would have to open the saved project every time
-    6. Then we will edit a specific branch of code, then when we are done, we save the code, then go back to GitHub desktop and commit it 
-          * branches are named gizmo, wheels, etc
-    7. After the coder commits their new code, the owner or someone will look at the code, and if it looks ok, then they can press 
+    1. Download the tools
+          Install GitHub Desktop.
+          Install Arduino IDE.
+    2. Get the repository
+          Go to our project on the GitHub website.
+          Click the green Code button (top-right area).
+          Choose Open with GitHub Desktop.
+          This will clone (copy) the repo (repository) onto your computer.
+    3. Open GitHub Desktop
+          It will take a little bit, but if it doesn’t open automatically, launch it yourself.
+          Choose “View files in your repository” to quickly open the folder where your Arduino project lives.
+    4. Open the project in Arduino IDE
+          Find the .ino file inside the cloned repo folder.
+          Open it with Arduino IDE.
+          Make sure you’re working in this repo folder (so GitHub can track changes).
+    5. Work on a branch
+          In GitHub Desktop, make or switch to a branch (e.g., gizmo, wheels, sensors).
+          Code and save changes in Arduino IDE.
+    6. Commit changes
+          Go back to GitHub Desktop.
+          You’ll see your changes under the Changes tab.
+          Write a short description of what you did (e.g., “Added motor speed control”).
+          Click Commit to [branch name].
+    7. Sync your work
+          Press Push origin (top bar) to upload your branch changes to GitHub online.
+    8. Review and merge
+          On the GitHub website, open a Pull Request for your branch.
+          Teammates review the code.
+          If it’s good, press Merge Pull Request to add it to main.
 
-  
+
+
 void loop() {
   // put your main code here, to run repeatedly:
 
