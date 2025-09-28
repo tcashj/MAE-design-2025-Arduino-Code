49647ad3241dea1e5066c371231ef684b3eee2e3
tcashj <tanner.jacob@okstate.edu>
2025-09-27 19:40:41 -0500

    Update MAE_Design_Fall2025.ino

--- MAE_Design_Fall2025/MAE_Design_Fall2025.ino
+++ MAE_Design_Fall2025/MAE_Design_Fall2025.ino
@@ -7,21 +7,20 @@ Collaborators can view? check
 
 arduino to github? Check
 
-Next step is to find out branchs?
-
-          pull request are for others to review/ see what changes you made to the code before adding it to the main branch
+The next step is to find out the branches.
+          Pull requests are for others to review/see what changes you made to the code before adding it to the main branch
 
 Steps to set up
     1. download github desktop and arduino ide
-    2. open github website and then click code (shoulde be a green button around the mid top right ish area)
-    3. click open with get hub dekstop (this clones the code from website to the app)
+    2. Open github website and then click Code (should be a green button around the mid top right-ish area)
+    3. Click open with GitHub Desktop (this clones the code from the website to the app)
           * should take a little bit to load
-    4. open github desktop if it doesnt open automatically
-    5. i use the "view files in your repository" option since the first option options up a blank project and 
-       then i would have to open the saved project everytime
-    6. then we will edit a specific branch of code then when we are done we save the code, then go back to githut desktop and commit it 
-          * branches are named gizmo, wheels, ect
-    7. after the coder commits their new code the owner or someone will look at the code and if it looks ok then they can press 
+    4. Open GitHub Desktop if it doesn't open automatically
+    5. I use the "view files in your repository" option since the first option options up a blank project and 
+       Then I would have to open the saved project every time
+    6. Then we will edit a specific branch of code, then when we are done, we save the code, then go back to GitHub desktop and commit it 
+          * branches are named gizmo, wheels, etc
+    7. After the coder commits their new code, the owner or someone will look at the code, and if it looks ok, then they can press 
 
   
 void loop() {
