9d6cdbfcad8908b0cd617d52abcaf80a7435da43
tcashj <tanner.jacob@okstate.edu>
2025-09-27 19:36:25 -0500

    Update MAE_Design_Fall2025.ino

--- MAE_Design_Fall2025/MAE_Design_Fall2025.ino
+++ MAE_Design_Fall2025/MAE_Design_Fall2025.ino
@@ -8,6 +8,7 @@ Collaborators can view? check
 arduino to github? Check
 
 Next step is to find out branchs?
+    alright, i created gizmo branch and now im editing the code in that branch.
   
 void loop() {
   // put your main code here, to run repeatedly:
