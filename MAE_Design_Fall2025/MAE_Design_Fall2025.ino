2df734a4b8e15ec6a21402374138b5be51eca3e6
tcashj <tanner.jacob@okstate.edu>
2025-09-27 20:04:35 -0500

    Update MAE_Design_Fall2025.ino

--- MAE_Design_Fall2025/MAE_Design_Fall2025.ino
+++ MAE_Design_Fall2025/MAE_Design_Fall2025.ino
@@ -11,21 +11,9 @@ The next step is to find out the branches.
           Pull requests are for others to review/see what changes you made to the code before adding it to the main branch
 
 
-Conflict resolution? check
+Conflict resolution? Check
 
 Steps to set up
-    1. Download github desktop and arduino ide
-    2. open github website and then click code (shoulde be a green button around the mid top right ish area)
-    3. click open with get hub dekstop (this clones the code from website to the app)
-          * should take a little bit to load
-    4. open github desktop if it doesnt open automatically
-    5. i use the "view files in your repository" option since the first option options up a blank project and 
-       then i would have to open the saved project everytime
-    6. then we will edit a specific branch of code then when we are done we save the code, then go back to githut desktop and commit it 
-          * branches are named gizmo, wheels, ect
-    7. after the coder commits their new code the owner or someone will look at the code and if it looks ok then they can press 
-
-  
     1. Download the tools
           Install GitHub Desktop.
           Install Arduino IDE.
@@ -50,7 +38,7 @@ Steps to set up
           Click Commit to [branch name].
     7. Sync your work
           Press Push origin (top bar) to upload your branch changes to GitHub online.
-          Press Create Pull requests 
+          Press Create Pull requests, and it will take you to the website
     8. Review and merge
           On the GitHub website, open a Pull Request for your branch.
           Teammates review the code.
